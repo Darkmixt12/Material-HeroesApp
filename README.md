2926d8a74e87bf0e8bda8906ba9a567772e87a5f
Steven Muñoz Vega <Darkmixt@gmail.com>
2023-09-24 13:47:21 -0600

    Hero project star from home

---
+++ README.md
@@ -0,0 +1,14 @@
+# HeroesApp
+
+## DEV
+
+1. Clonar el proyecto
+2. Ejectuar el ``npm install``
+3. Levantar backend ``npm run backend``
+4. Ejecutar la app ``npm start`` o el comando de ``ng server -o``
+
+5. Remplazar los archivos con los que inicia en produccion o desarrollo
+	               "fileReplacements": [
+                {"replace": "src/environments/environments.ts"},
+                {"with": "src/environments/environments.prod.ts"}
+              ],  
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+import { SearchHeroPageComponent } from './pages/search-hero-page/search-hero-page.component';
+import { MaterialModule } from '../material/material.module';
+
+
+@NgModule({
+  declarations: [
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewHeroPageComponent,
+    SearchHeroPageComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule,
+    MaterialModule
+  ]
+})
+export class HeroesModule { }
