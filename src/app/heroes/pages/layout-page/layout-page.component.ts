bec7612bb0e5b28ddcf81b0f8702b1cc341fdea9
Steven Muñoz <darkmixt@gmail.com>
2023-09-29 11:51:14 -0600

    Authentication GUARDS added

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -19,5 +22,20 @@ export class LayoutPageComponent {
       label: 'Buscar', icon: 'search', url: './search'
     },
   ]
+
+  constructor( 
+    private router : Router,
+    private authService: AuthService){}
+
+  get user(): User | undefined{
+    return this.authService.currentUser;
+  }
+
+
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
+
 
 }
